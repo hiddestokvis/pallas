1603ec15be5323f397c623e12b6b40541d16c21c
Hidde Stokvis <hidde@neverhide.nl>
2017-08-11 10:48:05 +0200

    add: TableDecorator test file

---
+++ test/Datasource/TableDecorator.ts
@@ -0,0 +1,35 @@
+import { expect } from 'chai';
+import 'mocha';
+import { Table } from '../../src/Datasource/TableDecorator';
+import { Model } from '../../src/Models/Model';
+import 'reflect-metadata';
+
+describe('Datasource decorators: Table Decorator', () => {
+  @Table('test_table')
+  class M extends Model {
+    public _id: string | null;
+    private _test: string | null;
+    public abc: Function;
+
+    constructor(
+      test
+    ) {
+      super();
+      this.test = test;
+    }
+
+    get test(): string {
+      return this._test;
+    }
+
+    set test(test: string) {
+      this._test = test;
+    }
+  }
+  it('Should add a table name to a model', () => {
+    const m: M = new M('test');
+    const tableName: string = Reflect.getMetadata('TableName', m.constructor);
+    expect(tableName).to.be.a('string');
+    expect(tableName).to.equal('test_table');
+  });
+});
