72b0285b2e9e3f403bb872a7fbe5c506e8011522
Hidde Stokvis <hidde@neverhide.nl>
2017-08-11 10:47:40 +0200

    fix: changed test definition format

--- test/Datasource/ColumnDecorator.ts
+++ test/Datasource/ColumnDecorator.ts
@@ -4,7 +4,7 @@ import { Column, PrimaryColumn } from '../../src/Datasource/ColumnDecorator';
 import { Model } from '../../src/Models/Model';
 import 'reflect-metadata';
 
-describe('Datasource decorators', () => {
+describe('Datasource decorators: Column Decorator', () => {
   class M extends Model {
     @PrimaryColumn('pk')
     public _id: string | null;
@@ -27,33 +27,31 @@ describe('Datasource decorators', () => {
       this._test = test;
     }
   }
-  describe('Column Decorator', () => {
-    it('Should have a list of attached columns', () => {
-      const m: M = new M('test');
-      const columns: string[] = Reflect.getMetadata('columns', m.constructor);
-      expect(columns).to.be.an('array');
-      expect(columns).to.have.lengthOf(2);
-    });
+  it('Should have a list of attached columns', () => {
+    const m: M = new M('test');
+    const columns: string[] = Reflect.getMetadata('columns', m.constructor);
+    expect(columns).to.be.an('array');
+    expect(columns).to.have.lengthOf(2);
+  });
 
-    it('Should have a primary key column', () => {
-      const m: M = new M('test');
-      const primaryKey: string = Reflect.getMetadata('PrimaryColumn', m.constructor);
-      const column: any = Reflect.getMetadata(primaryKey, m.constructor);
-      expect(primaryKey).to.be.a('string');
-      expect(column).to.have.all.keys('propertyKey', 'columnName', 'primaryKey');
-      expect(column.primaryKey).to.equal(true);
-      expect(column.propertyKey).to.equal('_id');
-      expect(column.columnName).to.equal('pk');
-    });
+  it('Should have a primary key column', () => {
+    const m: M = new M('test');
+    const primaryKey: string = Reflect.getMetadata('PrimaryColumn', m.constructor);
+    const column: any = Reflect.getMetadata(primaryKey, m.constructor);
+    expect(primaryKey).to.be.a('string');
+    expect(column).to.have.all.keys('propertyKey', 'columnName', 'primaryKey');
+    expect(column.primaryKey).to.equal(true);
+    expect(column.propertyKey).to.equal('_id');
+    expect(column.columnName).to.equal('pk');
+  });
 
-    it('Should have a test column, attached to test_db', () => {
-      const m: M = new M('test');
-      const columnIndex: string = Reflect.getMetadata('columns', m.constructor).pop();
-      const column: any = Reflect.getMetadata(columnIndex, m.constructor);
-      expect(column).to.have.all.keys('propertyKey', 'columnName', 'primaryKey');
-      expect(column.primaryKey).to.equal(false);
-      expect(column.propertyKey).to.equal('_test');
-      expect(column.columnName).to.equal('test_db');
-    });
+  it('Should have a test column, attached to test_db', () => {
+    const m: M = new M('test');
+    const columnIndex: string = Reflect.getMetadata('columns', m.constructor).pop();
+    const column: any = Reflect.getMetadata(columnIndex, m.constructor);
+    expect(column).to.have.all.keys('propertyKey', 'columnName', 'primaryKey');
+    expect(column.primaryKey).to.equal(false);
+    expect(column.propertyKey).to.equal('_test');
+    expect(column.columnName).to.equal('test_db');
   });
 });
